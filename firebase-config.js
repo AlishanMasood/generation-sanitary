6ca7dc5809b241b28cc2d723c56e8a8f39fdda8d
AlishanMasood <alishanmasood20@gmail.com>
2024-09-15 01:30:58 +0500

    firebase credentials are added

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDElj6QQ-2fVTL8c5xiIvferI8qX_x60kc",
+  authDomain: "generation-tiles-and-sanitary.firebaseapp.com",
+  projectId: "generation-tiles-and-sanitary",
+  storageBucket: "generation-tiles-and-sanitary.appspot.com",
+  messagingSenderId: "538924635878",
+  appId: "1:538924635878:web:a25ae8d2835122c4178d27",
+  measurementId: "G-ZF8G19JQVC",
 };
